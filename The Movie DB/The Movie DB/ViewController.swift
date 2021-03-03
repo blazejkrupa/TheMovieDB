355275295d61e709e468d6de6a2486bd912c2a32
Blazej Krupa <blazejkrupa93@gmail.com>
2021-03-03 12:15:11 +0100

    Create Project

---
+++ The Movie DB/The Movie DB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Movie DB
+//
+//  Created by Blazej Krupa on 3/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ The Movie DB/The Movie DBTests/The_Movie_DBTests.swift
@@ -0,0 +1,33 @@
+//
+//  The_Movie_DBTests.swift
+//  The Movie DBTests
+//
+//  Created by Blazej Krupa on 3/3/21.
+//
+
+import XCTest
+@testable import The_Movie_DB
+
+class The_Movie_DBTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
